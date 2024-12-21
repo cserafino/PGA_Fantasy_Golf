e7c799471a953b91e51f6fa9a2e90f2a133ddbc3
Clint Serafino <118832359+cserafino@users.noreply.github.com>
2024-12-20 22:41:29 -0500

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,15 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id"
+    apiKey: "AIzaSyD9Uk6Lxqx8aIjjqiMbDI5ZPBtWrIcxxAY",
+    authDomain: "pga-fantasy-golf.firebaseapp.com",
+    projectId: "pga-fantasy-golf",
+    storageBucket: "pga-fantasy-golf.firebasestorage.app",
+    messagingSenderId: "693571964008",
+    appId: "1:693571964008:web:c4f760b7a111407cd73fdd",
+    measurementId: "G-EQN56J6MD3"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig); 
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
